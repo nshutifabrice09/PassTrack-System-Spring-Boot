7a049f6beef099cb535d125cc8516b14559dfb4c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-10 11:56:49 +0200

    repository-updates-commit

--- src/main/java/com/passtrack/fullstack_backend/repository/GPSLocationRepository.java
+++ src/main/java/com/passtrack/fullstack_backend/repository/GPSLocationRepository.java
@@ -2,6 +2,8 @@ package com.passtrack.fullstack_backend.repository;
 
 import com.passtrack.fullstack_backend.model.GPSLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GPSLocationRepository extends JpaRepository <GPSLocation, Long> {
 }
--- src/main/java/com/passtrack/fullstack_backend/repository/PassengerRepository.java
+++ src/main/java/com/passtrack/fullstack_backend/repository/PassengerRepository.java
@@ -1,2 +1,9 @@
-package com.passtrack.fullstack_backend.repository;public interface PassengerRepository {
+package com.passtrack.fullstack_backend.repository;
+
+import com.passtrack.fullstack_backend.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository <Passenger, Long> {
 }
