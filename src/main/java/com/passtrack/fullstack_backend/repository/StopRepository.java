f467103bdeec1ea947998d2c921c78d9a95ca1c4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-10 11:58:18 +0200

    final-repository-updates-commit

--- src/main/java/com/passtrack/fullstack_backend/repository/StopRepository.java
+++ src/main/java/com/passtrack/fullstack_backend/repository/StopRepository.java
@@ -1,2 +1,7 @@
-package com.passtrack.fullstack_backend.repository;public interface StopRepository {
+package com.passtrack.fullstack_backend.repository;
+
+import com.passtrack.fullstack_backend.model.Stop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StopRepository extends JpaRepository <Stop, Long> {
 }
--- src/main/java/com/passtrack/fullstack_backend/repository/Trip.java
+++ src/main/java/com/passtrack/fullstack_backend/repository/Trip.java
@@ -1,2 +1,6 @@
-package com.passtrack.fullstack_backend.repository;public interface Trip {
+package com.passtrack.fullstack_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Trip extends JpaRepository <Trip, Long> {
 }
