12e29d299ebd8d6c70a4a8df14f291de94e53342
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-10 09:44:11 +0200

    annotation-updates

--- src/main/java/com/passtrack/fullstack_backend/model/Booking.java
+++ src/main/java/com/passtrack/fullstack_backend/model/Booking.java
@@ -1,10 +1,17 @@
 package com.passtrack.fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "bookings")
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/passtrack/fullstack_backend/model/Bus.java
+++ src/main/java/com/passtrack/fullstack_backend/model/Bus.java
@@ -1,8 +1,15 @@
 package com.passtrack.fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "buses")
 public class Bus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
