18f371a5b6a9ffb23aeefb72b924591f0b8be994
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-10 09:50:19 +0200

    repository-updates-commit

--- src/main/java/com/passtrack/fullstack_backend/model/Trip.java
+++ src/main/java/com/passtrack/fullstack_backend/model/Trip.java
@@ -1,11 +1,18 @@
 package com.passtrack.fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "trips")
 public class Trip {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/passtrack/fullstack_backend/model/User.java
+++ src/main/java/com/passtrack/fullstack_backend/model/User.java
@@ -1,8 +1,15 @@
 package com.passtrack.fullstack_backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
     @Id @GeneratedValue
     private Long id;
