4bba81aea70ddab35c47d892101f4a9f04f1cf65
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-10 12:11:49 +0200

    getters-setters-updates-commit

--- src/main/java/com/passtrack/fullstack_backend/model/Stop.java
+++ src/main/java/com/passtrack/fullstack_backend/model/Stop.java
@@ -16,8 +16,40 @@ public class Stop {
     private Long id;
     private String stopName;
     private String location;
-
     @ManyToOne
+    @JoinColumn(name = "route_id")
     private Route route;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStopName() {
+        return stopName;
+    }
+
+    public void setStopName(String stopName) {
+        this.stopName = stopName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
 }
 
