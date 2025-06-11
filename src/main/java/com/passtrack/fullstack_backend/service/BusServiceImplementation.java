2e5a7468ebe6ba1fd85f2cc2adb94af5d309c20d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-11 11:04:56 +0200

    service-impl-updates-commit

--- src/main/java/com/passtrack/fullstack_backend/service/BusServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/BusServiceImplementation.java
@@ -1,12 +1,28 @@
 package com.passtrack.fullstack_backend.service;
 
 import com.passtrack.fullstack_backend.model.Bus;
+import com.passtrack.fullstack_backend.repository.BusRepository;
+import com.passtrack.fullstack_backend.repository.DriverRepository;
+import com.passtrack.fullstack_backend.repository.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BusServiceImplementation implements BusService{
+
+    private final BusRepository busRepository;
+    private final DriverRepository driverRepository;
+    private final RouteRepository routeRepository;
+
+    @Autowired
+    public BusServiceImplementation(BusRepository busRepository, DriverRepository driverRepository, RouteRepository routeRepository) {
+        this.busRepository = busRepository;
+        this.driverRepository = driverRepository;
+        this.routeRepository = routeRepository;
+    }
+
     @Override
     public List<Bus> getAllBuses() {
         return null;
