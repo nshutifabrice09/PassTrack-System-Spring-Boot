efcade2c15f3239786e8bcda986417f16d4fbc76
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-11 09:47:02 +0200

    initial-service-commit

--- src/main/java/com/passtrack/fullstack_backend/service/BookingServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/BookingServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.Booking;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingServiceImplementation implements BookingService{
+    @Override
+    public List<Booking> getAllBookings() {
+        return null;
+    }
+
+    @Override
+    public Booking getBookingById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Booking save(Booking booking, Long passengerId, Long tripId) {
+        return null;
+    }
+
+    @Override
+    public Booking update(Booking booking, Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
