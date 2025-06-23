b7e3c8f85fdb18f426eb65ba166502fe0672e743
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-23 22:55:42 +0200

    parameters-update-commit

--- src/main/java/com/passtrack/fullstack_backend/service/DriverService.java
+++ src/main/java/com/passtrack/fullstack_backend/service/DriverService.java
@@ -1,2 +1,13 @@
-package com.passtrack.fullstack_backend.service;public interface DriverService {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.Driver;
+
+import java.util.List;
+
+public interface DriverService {
+    List<Driver> getAllDrivers();
+    Driver getDriverById(Long id);
+    Driver saveDriver(Driver driver, Long busId);
+    Driver updateDriver(Long id, Driver driver);
+    void removeDriver(Long id);
 }
