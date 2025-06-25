79e42a5932fbbf53c5622c5c52fa21433b3b7641
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 10:43:53 +0200

    route-service-impl-commit

--- src/main/java/com/passtrack/fullstack_backend/service/PassengerServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/PassengerServiceImplementation.java
@@ -1,7 +1,53 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.Passenger;
+import com.passtrack.fullstack_backend.repository.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerServiceImplementation implements PassengerService{
+
+    private PassengerRepository passengerRepository;
+
+    @Autowired
+    public PassengerServiceImplementation (PassengerRepository passengerRepository){
+        this.passengerRepository= passengerRepository;
+    }
+
+    @Override
+    public List<Passenger> getAllPassengers() {
+        return passengerRepository.findAll();
+    }
+
+    @Override
+    public Passenger getPassengerById(Long id) {
+        return passengerRepository.findPassengerById(id);
+    }
+
+    @Override
+    public Passenger savePassenger(Passenger passenger) {
+        return passengerRepository.save(passenger);
+    }
+
+    @Override
+    public Passenger updatePassenger(Long id, Passenger passenger) {
+        Passenger existPassenger = passengerRepository.findPassengerById(id);
+        if(existPassenger !=null){
+            existPassenger.setFullName(passenger.getFullName());
+            existPassenger.setPhoneNumber(passenger.getPhoneNumber());
+            existPassenger.setEmail(passenger.getEmail());
+            existPassenger.setLanguagePreference(passenger.getLanguagePreference());
+            existPassenger.setAccessibilitySupport(passenger.isAccessibilitySupport());
+            return passengerRepository.save(existPassenger);
+        }
+        return null;
+    }
+
+    @Override
+    public void removePassenger(Long id) {
+        passengerRepository.deleteById(id);
+    }
 }
