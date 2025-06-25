6c3e6de353e00646e8431c6371c2534b54b53e95
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 09:39:41 +0200

    repo-updates-commit

--- src/main/java/com/passtrack/fullstack_backend/service/GPSLocationServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/GPSLocationServiceImplementation.java
@@ -1,7 +1,47 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.GPSLocation;
+import com.passtrack.fullstack_backend.repository.GPSLocationRepository;
+import com.passtrack.fullstack_backend.repository.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GPSLocationServiceImplementation implements GPSLocationService{
+
+    private final GPSLocationRepository gpsLocationRepository;
+    private final TripRepository tripRepository;
+
+    @Autowired
+    public GPSLocationServiceImplementation(GPSLocationRepository gpsLocationRepository, TripRepository tripRepository) {
+        this.gpsLocationRepository = gpsLocationRepository;
+        this.tripRepository = tripRepository;
+    }
+
+    @Override
+    public List<GPSLocation> getGpsLocations() {
+        return null;
+    }
+
+    @Override
+    public GPSLocation getGpsLocationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public GPSLocation saveGpsLocation(GPSLocation gpsLocation, Long tripId) {
+        return null;
+    }
+
+    @Override
+    public GPSLocation updateGpsLocation(Long id, GPSLocation gpsLocation) {
+        return null;
+    }
+
+    @Override
+    public void removeGPSLocation(Long id) {
+
+    }
 }
