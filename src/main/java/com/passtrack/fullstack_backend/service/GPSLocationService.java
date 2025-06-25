ff42e9a6480fdbd08107b06ef748b4156d4d396c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 09:38:52 +0200

    gps-service-impl-paramters-commit

--- src/main/java/com/passtrack/fullstack_backend/service/GPSLocationService.java
+++ src/main/java/com/passtrack/fullstack_backend/service/GPSLocationService.java
@@ -1,2 +1,13 @@
-package com.passtrack.fullstack_backend.service;public interface GPSLocationService {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.GPSLocation;
+
+import java.util.List;
+
+public interface GPSLocationService {
+    List<GPSLocation> getGpsLocations();
+    GPSLocation getGpsLocationById(Long id);
+    GPSLocation saveGpsLocation(GPSLocation gpsLocation, Long tripId);
+    GPSLocation updateGpsLocation(Long id, GPSLocation gpsLocation);
+    void removeGPSLocation(Long id);
 }
