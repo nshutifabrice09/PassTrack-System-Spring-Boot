d0bde4e1b4423bbd4a691e7ea5971ba369d9481a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 10:14:05 +0200

    notification-service-impl-constructor-parameters-commit

--- src/main/java/com/passtrack/fullstack_backend/service/NotificationServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/NotificationServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.Notification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotificationServiceImplementation implements NotificationService{
+    @Override
+    public List<Notification> getNotifications() {
+        return null;
+    }
+
+    @Override
+    public Notification getNotificationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Notification saveNotification(Notification notification, Long passengerId) {
+        return null;
+    }
+
+    @Override
+    public Notification updateNotification(Long id, Notification notification) {
+        return null;
+    }
+
+    @Override
+    public void removeNotification(Long id) {
+
+    }
 }
