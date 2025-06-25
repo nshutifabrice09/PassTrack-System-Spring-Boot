65ce9bb468827145e0be6b8481bf055f37c325de
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 20:01:07 +0200

    route-service-impl-commit

--- src/main/java/com/passtrack/fullstack_backend/service/RouteServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/RouteServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.passtrack.fullstack_backend.service;public class RouteServiceImplementation {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.Route;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RouteServiceImplementation implements RouteService{
+    @Override
+    public List<Route> getAllRoutes() {
+        return null;
+    }
+
+    @Override
+    public Route getRouteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Route saveRoute(Route route) {
+        return null;
+    }
+
+    @Override
+    public Route updateRoute(Long id, Route route) {
+        return null;
+    }
+
+    @Override
+    public void removeRoute(Long id) {
+
+    }
 }
