38ab94a207deb03a62a8432d2c7a099bc587eac3
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 10:12:30 +0200

    notification-service-impl-methods-commit

--- src/main/java/com/passtrack/fullstack_backend/service/NotificationService.java
+++ src/main/java/com/passtrack/fullstack_backend/service/NotificationService.java
@@ -1,2 +1,13 @@
-package com.passtrack.fullstack_backend.service;public interface NotificationService {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.Notification;
+
+import java.util.List;
+
+public interface NotificationService {
+    List<Notification> getNotifications();
+    Notification getNotificationById(Long id);
+    Notification saveNotification(Notification notification, Long passengerId);
+    Notification updateNotification(Long id, Notification notification);
+    void removeNotification(Long id);
 }
