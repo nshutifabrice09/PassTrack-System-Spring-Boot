03865c466ee61a33033f154065a3a31bd13c71a7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-25 10:44:34 +0200

    route-service-impl-methods-commit

--- src/main/java/com/passtrack/fullstack_backend/service/RouteService.java
+++ src/main/java/com/passtrack/fullstack_backend/service/RouteService.java
@@ -1,2 +1,13 @@
-package com.passtrack.fullstack_backend.service;public interface RouteService {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.Route;
+
+import java.util.List;
+
+public interface RouteService {
+    List<Route> getAllRoutes();
+    Route getRouteById(Long id);
+    Route saveRoute(Route route);
+    Route updateRoute(Long id, Route route);
+    void removeRoute(Long id);
 }
