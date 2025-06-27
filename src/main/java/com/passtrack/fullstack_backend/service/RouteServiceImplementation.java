836aad3c3bb55890377044aae79519e183eea310
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-27 10:11:35 +0200

    serviceAlert-impl-commit

--- src/main/java/com/passtrack/fullstack_backend/service/RouteServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/RouteServiceImplementation.java
@@ -1,34 +1,49 @@
 package com.passtrack.fullstack_backend.service;
 
 import com.passtrack.fullstack_backend.model.Route;
+import com.passtrack.fullstack_backend.repository.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RouteServiceImplementation implements RouteService{
+
+    private final RouteRepository routeRepository;
+
+    @Autowired
+    public RouteServiceImplementation(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
+    }
+
     @Override
     public List<Route> getAllRoutes() {
-        return null;
+        return routeRepository.findAll();
     }
 
     @Override
     public Route getRouteById(Long id) {
-        return null;
+        return routeRepository.findRouteById(id);
     }
 
     @Override
     public Route saveRoute(Route route) {
-        return null;
+        return routeRepository.save(route);
     }
 
     @Override
     public Route updateRoute(Long id, Route route) {
+        Route existRoute = routeRepository.findRouteById(id);
+        if(existRoute !=null){
+            existRoute.setRouteName(route.getRouteName());
+            return routeRepository.save(existRoute);
+        }
         return null;
     }
 
     @Override
     public void removeRoute(Long id) {
-
+        routeRepository.deleteById(id);
     }
 }
---
+++ src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertServiceImplementation.java
@@ -0,0 +1,2 @@
+package com.passtrack.fullstack_backend.service;public class ServiceAlertServiceImplementation {
+}
