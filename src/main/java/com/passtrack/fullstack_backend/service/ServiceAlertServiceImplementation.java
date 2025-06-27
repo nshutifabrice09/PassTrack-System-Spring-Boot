10d07ecab6459b306ea26f2e0d64df6d04c09768
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-27 10:20:20 +0200

    serviceAlert-repo-updates

--- src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertServiceImplementation.java
@@ -1,7 +1,44 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.ServiceAlert;
+import com.passtrack.fullstack_backend.repository.ServiceAlertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ServiceAlertServiceImplementation implements ServiceAlertService{
+
+    private final ServiceAlertRepository serviceAlertRepository;
+
+    @Autowired
+    public ServiceAlertServiceImplementation (ServiceAlertRepository serviceAlertRepository){
+        this.serviceAlertRepository= serviceAlertRepository;
+    }
+
+    @Override
+    public List<ServiceAlert> getAllServiceAlerts() {
+        return null;
+    }
+
+    @Override
+    public ServiceAlert getServiceAlertById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ServiceAlert saveServiceAlert(ServiceAlert serviceAlert) {
+        return null;
+    }
+
+    @Override
+    public ServiceAlert updateServiceAlert(Long id, ServiceAlert serviceAlert) {
+        return null;
+    }
+
+    @Override
+    public void removeServiceAlert(Long id) {
+
+    }
 }
