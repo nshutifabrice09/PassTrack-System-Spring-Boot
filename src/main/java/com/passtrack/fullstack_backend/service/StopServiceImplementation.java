c5ac8d870fd779a373cd0616fe255d303a8f5d2e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-27 10:47:42 +0200

    stopservice-impl-updates

--- src/main/java/com/passtrack/fullstack_backend/service/StopServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/StopServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.Stop;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StopServiceImplementation implements StopService{
+    @Override
+    public List<Stop> getAllStops() {
+        return null;
+    }
+
+    @Override
+    public Stop getStopById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Stop saveStop(Stop stop, Long routeId) {
+        return null;
+    }
+
+    @Override
+    public Stop updateStop(Long id, Stop stop) {
+        return null;
+    }
+
+    @Override
+    public void removeStop(Long id) {
+
+    }
 }
