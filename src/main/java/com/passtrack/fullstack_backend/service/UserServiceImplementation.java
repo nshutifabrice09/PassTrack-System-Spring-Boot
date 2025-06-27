7bac4df4b0775ab533fabc41f08847601138750c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-27 13:29:01 +0200

    user-repo-updates

--- src/main/java/com/passtrack/fullstack_backend/service/UserServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/UserServiceImplementation.java
@@ -1,2 +1,7 @@
-package com.passtrack.fullstack_backend.service;public class UserServiceImplementation {
+package com.passtrack.fullstack_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImplementation implements UserService{
 }
