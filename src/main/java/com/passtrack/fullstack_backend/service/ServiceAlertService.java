204d0f98f93bd4e36fc0915e35926aad708d0234
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-27 10:12:39 +0200

    serviceAlert-impl-commit

--- src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertService.java
+++ src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertService.java
@@ -1,2 +1,14 @@
-package com.passtrack.fullstack_backend.service;public interface ServiceAlertService {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.ServiceAlert;
+
+import java.util.List;
+
+public interface ServiceAlertService {
+
+    List<ServiceAlert> getAllServiceAlerts();
+    ServiceAlert getServiceAlertById(Long id);
+    ServiceAlert saveServiceAlert(ServiceAlert serviceAlert);
+    ServiceAlert updateServiceAlert(Long id, ServiceAlert serviceAlert);
+    void removeServiceAlert(Long id);
 }
--- src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/ServiceAlertServiceImplementation.java
@@ -1,2 +1,7 @@
-package com.passtrack.fullstack_backend.service;public class ServiceAlertServiceImplementation {
+package com.passtrack.fullstack_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceAlertServiceImplementation implements ServiceAlertService{
 }
