d714672093c2cdbc2b2fbc3c415724871d25e7a7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-27 10:38:58 +0200

    stopservice-impl-updates

--- src/main/java/com/passtrack/fullstack_backend/service/StopService.java
+++ src/main/java/com/passtrack/fullstack_backend/service/StopService.java
@@ -1,2 +1,13 @@
-package com.passtrack.fullstack_backend.service;public interface StopService {
+package com.passtrack.fullstack_backend.service;
+
+import com.passtrack.fullstack_backend.model.Stop;
+
+import java.util.List;
+
+public interface StopService {
+    List<Stop> getAllStops();
+    Stop getStopById(Long id);
+    Stop saveStop(Stop stop, Long routeId);
+    Stop updateStop(Long id, Stop stop);
+    void removeStop(Long id);
 }
