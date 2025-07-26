2d570be49c29c5c4baf2a8af872dcf450e593c93
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-26 17:39:55 +0200

    initial-bookingController-commit

--- src/main/java/com/passtrack/fullstack_backend/service/UserServiceImplementation.java
+++ src/main/java/com/passtrack/fullstack_backend/service/UserServiceImplementation.java
@@ -1,5 +1,7 @@
 package com.passtrack.fullstack_backend.service;
 
+import com.passtrack.fullstack_backend.model.Driver;
+import com.passtrack.fullstack_backend.model.Passenger;
 import com.passtrack.fullstack_backend.model.User;
 import com.passtrack.fullstack_backend.repository.DriverRepository;
 import com.passtrack.fullstack_backend.repository.PassengerRepository;
@@ -36,16 +38,29 @@ public class UserServiceImplementation implements UserService{
 
     @Override
     public User saveUser(User user, Long passengerId, Long driverId) {
-        return null;
+        Passenger passenger = passengerRepository.findPassengerById(passengerId);
+        Driver driver = driverRepository.findDriverById(driverId);
+        user.setPassenger(passenger);
+        user.setDriver(driver);
+        return userRepository.save(user);
     }
 
     @Override
     public User updateUser(Long id, User user) {
+        User existUser = userRepository.findUserById(id);
+        if(existUser != null){
+            existUser.setUsername(user.getUsername());
+            existUser.setPassword(user.getUsername());
+            existUser.setEmail(user.getEmail());
+            existUser.setRole(user.getRole());
+            existUser.setEnabled(user.isEnabled());
+            return userRepository.save(existUser);
+        }
         return null;
     }
 
     @Override
     public void removeUser(Long id) {
-
+        userRepository.deleteById(id);
     }
 }
