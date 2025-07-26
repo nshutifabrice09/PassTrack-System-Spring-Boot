2e956e5aae6a0169648cdd2bb054a0fc648bbf41
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-26 17:48:40 +0200

    initial-passengerController-commit

--- src/main/java/com/passtrack/fullstack_backend/controller/BookingController.java
+++ src/main/java/com/passtrack/fullstack_backend/controller/BookingController.java
@@ -1,2 +1,26 @@
-package com.passtrack.fullstack_backend.controller;public class BookingController {
+package com.passtrack.fullstack_backend.controller;
+
+
+import com.passtrack.fullstack_backend.model.Booking;
+import com.passtrack.fullstack_backend.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService){
+        this.bookingService= bookingService;
+    }
+
+    @PostMapping("/booking/{passengerId}/{tripId}")
+    public Booking saveBooking(@RequestBody Booking booking, @PathVariable ("PassengerId") Long passengerId, @PathVariable ("tripId") Long tripId){
+        return bookingService.save(booking, passengerId, tripId);
+    }
+    
+
 }
