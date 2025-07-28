39d74a1fbc365fdad7d0ffcf84986d205732ae53
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-28 13:40:35 +0200

    update-busController-commit

--- src/main/java/com/passtrack/fullstack_backend/controller/BusController.java
+++ src/main/java/com/passtrack/fullstack_backend/controller/BusController.java
@@ -1,2 +1,16 @@
-package com.passtrack.fullstack_backend.controller;public class BusController {
+package com.passtrack.fullstack_backend.controller;
+
+import com.passtrack.fullstack_backend.service.BusService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class BusController {
+
+    private final BusService busService;
+
+    public BusController(BusService busService) {
+        this.busService = busService;
+    }
 }
