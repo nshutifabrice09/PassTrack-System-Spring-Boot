7dcc46dd58b2b9239a1afd7e94602bab763751cb
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-28 10:35:43 +0200

    update-stopController-commit

--- src/main/java/com/passtrack/fullstack_backend/controller/StopController.java
+++ src/main/java/com/passtrack/fullstack_backend/controller/StopController.java
@@ -1,20 +1,31 @@
 package com.passtrack.fullstack_backend.controller;
 
+import com.passtrack.fullstack_backend.model.Stop;
 import com.passtrack.fullstack_backend.service.StopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class StopController {
 
-    private final StopService service;
+    private final StopService stopService;
 
     @Autowired
-    public StopController(StopService service) {
-        this.service = service;
+    public StopController(StopService stopService){
+        this.stopService = stopService;
     }
 
-    
+    @PostMapping("/stop/{routeId}")
+    public Stop save(@RequestBody Stop stop, @PathVariable ("routeId") Long routeId){
+        return stopService.saveStop(stop, routeId);
+    }
+
+//    @GetMapping("/routes")
+//    public List<Stop> stopList(){
+//        return stopService.getAllStops();
+//    }
+
 }
